0539803c35b9dd0d244b898949820822e898bfcb
RobelAraia <robelaraia@hotmail.com>
2024-02-14 02:21:50 +0000

    created azure function api

--- frontend/main.js
+++ frontend/main.js
@@ -1,19 +1,22 @@
-window.addEventListener('DOMcontentLoaded', (event) => {
+window.addEventListener("DOMContentLoaded", (event) => {
     getVisitCount();
-})
-
-const functionApi = '';
-
-const getVisitCount = () => {
+  });
+  
+  const functionAPI = "http://localhost:7071/api/GetResumeCounter";
+  
+  const getVisitCount = async () => {
     let count = 30;
-    fetch(functionApi).then(response => {
-        return response.json()
-    }).then(response =>{
-        console.log("website called function API.");
+    await fetch(functionAPI)
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        console.log("Website called function API.");
         count = response.count;
         document.getElementById("counter").innerText = count;
-    }).catch(function(error){
+      })
+      .catch(function (error) {
         console.log(error);
-    });
+      });
     return count;
-}
+  };
